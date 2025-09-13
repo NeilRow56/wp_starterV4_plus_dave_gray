b5756404908faf4e47c58174cbc248bec1a307f5
NeilRow56 <neilrowland56@gmail.com>
2025-09-13 13:25:15 +0100

    first commit

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,34 @@
+import { inferAdditionalFields, adminClient } from 'better-auth/client/plugins'
+import { createAuthClient } from 'better-auth/react'
+import type { auth } from '@/lib/auth'
+import { ac, roles } from '@/lib/permissions'
+
+const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  plugins: [inferAdditionalFields<typeof auth>(), adminClient({ ac, roles })]
+})
+
+// You can also check multiple resource permissions at the same time
+
+export const canCreateProject = await authClient.admin.hasPermission({
+  permissions: {
+    project: ['create']
+
+    // sale: ["create"]
+  },
+
+  role: 'manager'
+})
+
+export const {
+  signIn,
+  signUp,
+  signOut,
+  useSession,
+  getSession,
+  admin,
+  sendVerificationEmail,
+  forgetPassword,
+  resetPassword,
+  updateUser
+} = authClient
